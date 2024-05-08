f650ae579485116a663d5f1cb82efb4e5ead79c8
FarukhMushtaq <farukhmushtaq@apesquared.com>
2024-05-08 17:09:05 +0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+while True:
+    user_action = input("add, show , edit, completed exit : ")
+    match user_action:
+        case 'add':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
+            todo = input("To-do: ")+"\n"
+            todos.append(todo)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
+        case 'show' | 'display':
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            for index, item in enumerate(todos):
+                print(f"{index+1}: {item.strip()}")
+        case 'edit':
+            itemNum = int(input("Which Item number you want to Edit: "))
+            todos[itemNum-1] = input("Enter new To-do: ")
+        case 'completed':
+            itemNum = int(input("Which Item number you want to mark Completed: "))
+            todos.pop(itemNum-1)
+        case 'exit':
+            break
+        case anyVarName:
+            print("You entered invalid case to match",anyVarName)
+print('Bye')
