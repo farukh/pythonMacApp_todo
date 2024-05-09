92ce868dc3cc1df77a4cffdf18e4acacb5d83dd5
FarukhMushtaq <farukhmushtaq@apesquared.com>
2024-05-09 10:06:47 +0500

    Initial Commit - Basic GUI

--- pythonProjects/FileCompressor/GuiMain.py
+++ pythonProjects/FileCompressor/GuiMain.py
@@ -1,14 +1,14 @@
 import FreeSimpleGUI as Sg
-lblSelectFiles = Sg.Text("Select files for compression")
+lblSelectFiles = Sg.Text("Select Files to Compress: ")
 filesSelector = Sg.InputText()
 btnFileChooser = Sg.FileBrowse()
 
-lblSelectDestinationFolder = Sg.Text("Select Destination Folder")
+lblSelectDestinationFolder = Sg.Text("Select Destination Folder: ")
 destFolderInput = Sg.Input()
 btnDestFolderSelector = Sg.FolderBrowse()
 
-btnCompressNow = Sg.Button("Compress Now.")
-window = Sg.Window("Supper File Compression", layout=[[lblSelectFiles,filesSelector,btnFileChooser],
+btnCompressNow = Sg.Button("Compress")
+window = Sg.Window("Supper File Compression ", layout=[[lblSelectFiles,filesSelector,btnFileChooser],
                                              [lblSelectDestinationFolder,destFolderInput,btnDestFolderSelector],
                                              [btnCompressNow]])
 window.read()
